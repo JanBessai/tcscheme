9162cb20daa40ba9c3c799fe604e640139cd7205
JanBessai <jan.bessai@tu-dortmund.de>
2020-01-02 09:21:55 +0100

    Clean up code

--- stl.scm
+++ stl.scm
@@ -1,112 +1,113 @@
-(require-extension srfi-1)
+;; Types
+(define (atomic? A) (symbol? A))
 
-(define (type? A)
-  (cond 
-    ((list? A)
-     (and (equal? 3 (length A))
-          (equal? '-> (car A))
-          (every type? (cdr A))))
-    (symbol? A)))
+(define (source A) (car (cdr A)))
+(define (target A) (car (cdr (cdr A))))
+(define (arrow? A)
+  (and (list? A)
+       (equal? 3 (length A))
+       (equal? '-> (car A))))
 
-(define (binder-pair? kv)
-  (and (equal? 2 (length kv))
-       (let ((bound-var (car kv))
-             (ascribed-type (car (cdr kv))))
-         (and (symbol? bound-var)
-              (type? ascribed-type)))))
+(define (type? A) (or (arrow? A) (atomic? A)))
 
-(define (with-target-for-source report-error todo S A)
-  (if (and (list? A)
-           (equal? 3 (length A))
-           (equal? '-> (car A))
-           (equal? S (car (cdr A))))
-      (todo (car (cdr (cdr A))))
-      (report-error (list 'expected (list '-> S 'Y) 'got A))))
+;; Terms
+(define (variable? M) (symbol? M))
+
+(define (bound-var b) (car b))
+(define (ascribed-type b) (car (cdr b)))
+(define (binder-pair? b)
+  (and (list? b)
+       (equal? 2 (length b))
+       (and (variable? (bound-var b))
+            (type? (ascribed-type b)))))
+
+(define (functional M) (car M))
+(define (argument M) (car (cdr M)))
+(define (application? M)
+  (and (list? M)
+       (equal? 2 (length M))))
+
+(define (binder M) (car (cdr M)))
+(define (body M) (car (cdr (cdr M))))
+(define (lambda? M)
+  (and (list? M)
+       (equal? 3 (length M))
+       (equal? 'lambda (car M))
+       (binder-pair? (binder M))))
+
+(define (type-in report-error todo Gamma x)
+  (let ((res (assoc x Gamma)))
+    (if res 
+        (todo (car (cdr res)))
+        (report-error (list 'unbound-variable x)))))
 
 (define (synthesize report-error todo Gamma M)
   (cond 
-    ((symbol? M)
-     (let 
-       ((binding (find (lambda (kv) (equal? (car kv) M)) Gamma)))
-       (if (not binding) 
-           (report-error (list 'unbound-variable M))
-           (todo (car (cdr binding))))))
-    ((and (equal? 3 (length M))
-          (equal? 'lambda (car M))
-          (binder-pair? (car (cdr M))))
-     (let* ((binder (car (cdr M)))
-            (bound-type (car (cdr binder)))
-            (body (car (cdr (cdr M)))))
-           (synthesize
-             report-error
-             (lambda (T) (todo (list '-> bound-type T)))
-             (cons binder Gamma)
-             body)))
-    ((equal? 2 (length M))
+    ((variable? M)
+     (type-in report-error todo Gamma M))
+    ((lambda? M)
+     (synthesize report-error 
+                 (lambda (T) (todo (list '-> (ascribed-type (binder M)) T)))
+                 (cons (binder M) Gamma) 
+                 (body M)))
+    ((application? M)
      (synthesize 
        report-error
        (lambda (ST) 
-          (if (and (list? ST)
-                   (equal? 3 (length ST))
-                   (equal? '-> (car ST)))
-              (check 
-                report-error 
-                (lambda (ok) (todo (car (cdr (cdr ST)))))
-                Gamma
-                (car (cdr M))
-                (car (cdr ST)))
-              (synthesize
-                report-error
-                (lambda (S) (report-error (list 'expected (list '-> S 'Y) 'got ST)))
-                Gamma
-                (car (cdr M)))))
+          (if (arrow? ST)
+              (check report-error 
+                     (lambda (ok) (todo (target ST))) 
+                     Gamma 
+                     (argument M)
+                     (source ST))
+              (synthesize report-error
+                          (lambda (S)
+                            (report-error 
+                              (list 'expected (list '-> S 'SomeType) 'got ST)))
+                          Gamma
+                          (argument M))))
        Gamma
-       (car M)))
+       (functional M)))
     (report-error (list 'unknown-statement M))))
 
 (define (check report-error todo Gamma M A)
   (cond 
-    ((symbol? M)
-     (let 
-       ((binding (find (lambda (kv) (equal? (car kv) M)) Gamma)))
-       (cond ((not binding) (report-error (list 'unbound-variable M)))
-             ((equal? A (car (cdr binding))) (todo #t))
-             (report-error (list 'expected A 'got (car (cdr binding)))))))
-    ((and (equal? 3 (length M))
-          (equal? 'lambda (car M))
-          (binder-pair? (car (cdr M))))
-     (let* ((binder (car (cdr M)))
-            (bound-type (car (cdr binder)))
-            (body (car (cdr (cdr M)))))
-           (with-target-for-source 
-             report-error
-             (lambda (T) (check report-error todo (cons binder Gamma) body T))
-             bound-type
-             A)))
-    ((equal? 2 (length M))
-     (synthesize 
+    ((variable? M)
+     (type-in 
        report-error
-       (lambda (ST) 
-          (if (and (list? ST)
-                   (equal? 3 (length ST))
-                   (equal? '-> (car ST)))
-              (check 
-                report-error 
-                (lambda (ok)
-                  (let ((T (car (cdr (cdr ST)))))
-                       (if (equal? A T)
-                           (todo #t)
-                           (report-error (list 'expected A 'got T)))))
-                Gamma
-                (car (cdr M))
-                (car (cdr ST)))
-              (synthesize 
-                report-error
-                (lambda (S) (report-error (list 'expected (list '-> S A) 'got ST)))
-                Gamma
-                (car (cdr M)))))
+       (lambda (T)
+          (if (equal? A T) 
+              (todo #t)
+              (report-error (list 'expected A 'got T))))
        Gamma
-       (car M)))
+       M))
+    ((lambda? M)
+     (let ((S (ascribed-type (binder M))))
+       (if (and (arrow? A) (equal? (source A) S))
+         (check report-error todo (cons (binder M) Gamma) (body M) (target A))
+         (report-error (list 'expected A 'got (list '-> S 'SomeType))))))
+    ((application? M)
+     (synthesize
+       report-error
+       (lambda (ST)
+          (if (arrow? ST)
+              (let ((T (target ST)))
+                (check report-error 
+                       (lambda (ok)
+                         (if (equal? A T)
+                           (todo #t)
+                           (report-error (list 'expected A 'got T))))
+                       Gamma
+                       (argument M)
+                       (source ST)))
+              (synthesize report-error
+                          (lambda (S) 
+                            (report-error 
+                              (list 'expected (list '-> S A) 'got ST)))
+                          Gamma
+                (argument M))))
+       Gamma
+       (functional M)))
     (report-error (list 'unknown-statement M))))
 
 (define (id x) x)
